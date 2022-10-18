2eb6a6f167faa8ffa591b3ccfc0d546cddda7172
YouSooA <kinsk2839@naver.com>
2022-10-18 18:02:39 +0900

    refector: 에러 메세지 호출 함수 false 반환하도록 수정

--- src/index.js
+++ src/index.js
@@ -5,19 +5,19 @@ export default function App() {
     $('#todo-input').value = '';
     $('#todo-input').focus();
     $('#thema-select').options[0].selected = true;
-  }
+  };
   const checkInput = (todo) => {
     if (todo.replace(/\s/g, '') === '') {
+      resetInput();
       alert('다시 todo를 입력해주세요.');
-      return resetInput();
+      return false;
     }
+    return true;
   };
   const addTodo = () => {
     const todo = $('#todo-input').value;
     const thema = $('#thema-select').options[$('#thema-select').selectedIndex].text;
-    checkInput(todo);
     resetInput();
-    console.log(todo, thema);
   };
 
   $('#todo-form').addEventListener('submit', (e) => {
